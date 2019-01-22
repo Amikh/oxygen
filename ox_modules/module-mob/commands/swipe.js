c626666d88a1097c08e48d75d063f0b36c5cec29
romovs <romovs@gmail.com>
2019-01-22 18:18:23 +0300

    Rework mob.swipe using touchAction

--- ox_modules/module-mob/commands/swipe.js
+++ ox_modules/module-mob/commands/swipe.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2018 CloudBeat Limited
+ * Copyright (C) 2015-2019 CloudBeat Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,29 +13,54 @@
  * @param {String=} locator - Locator of the element to swipe on.
  * @param {Number} xoffset - Horizontal offset.
  * @param {Number} yoffset - Vertical offset. Negative value indicates swipe down and positive indicates swipe up direction.
- * @param {Number=} speed - The speed of swiping in pixels per second. Default is 30.
+ * @param {Number=} xstart - Absolute X coordinate to start the swipe from.
+ * @param {Number=} ystart - Absolute Y coordinate to start the swipe from.
  * @for android, ios
-*/
-module.exports = function(locator, xoffset, yoffset, speed) {
+ * @example <caption>[javascript] Usage example</caption>
+ * // swipe 500 pixels down using an element as a starting point
+ * mob.swipe('id=com.package.app:id/element_id', 0, -500);
+ * // swipe 500 pixels down using absolute coordinates (50x300) as a starting point
+ * mob.swipe(0, -500, 10, 300);
+ */
+module.exports = function(locator, xoffset, yoffset, xstart, ystart) {
     this.helpers._assertArgument(locator, 'locator');
     this.helpers._assertArgumentNumber(xoffset, 'xoffset');
     this.helpers._assertArgumentNumber(yoffset, 'yoffset');
 
-    speed = typeof speed === 'number' ? speed : 30;
-    if (typeof locator === 'number' && typeof xoffset === 'number') {
-        yoffset = xoffset;
-        xoffset = locator;
-        locator = null;
-    }
-    if (locator != null) {
-        var elm = null;
-        if (typeof locator === 'object') {
-            elm = locator;
-        } else {
-            elm = this.findElement(locator);
+    if (typeof locator === 'number') {
+        // swipe using absolute coordinates as a starting point
+        this.helpers._assertArgumentNumber(arguments[2] /*xstart*/, 'xstart');
+        this.helpers._assertArgumentNumber(arguments[3] /*ystart*/, 'ystart');
+        this.driver.touchAction(
+            [
+                { action: 'press', x: arguments[2] /*xstart*/, y: arguments[3] /*ystart*/ },
+                { action: 'moveTo', x: arguments[0] /*xoffset*/, y: arguments[1] /*yoffset*/ },
+                'release'
+            ]
+        );
+    } else {
+        // swipe using an element as a starting point
+        if (typeof locator === 'object' && locator.touchAction) {
+            locator.touchAction(
+                [
+                    'press',
+                    { action: 'moveTo', x: xoffset, y: yoffset },
+                    'release'
+                ]
+            );
+            return;
         }
 
-        return elm.swipe(xoffset, yoffset, speed);
+        if (this.autoWait) {
+            this.waitForExist(locator);
+        }
+        this.driver.touchAction(
+            this.helpers.getWdioLocator(locator),
+            [
+                'press',
+                { action: 'moveTo', x: xoffset, y: yoffset },
+                'release'
+            ]
+        );
     }
-    return this.driver.swipe(xoffset, yoffset);
 };
